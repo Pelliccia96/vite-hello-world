20f79fd68be76b0e92d12e1327f6e6abaf313881
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-02 10:10:35 +0100

    Removed default style.css

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="my-5">
+    <div class="my-5 text-center">
         <img class="img-fluid" src="https://hiringplatform.boolean.careers/images/logo.png" alt="Boolean Logo">
     </div>
 </template>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container text-center">
+    <div class="container text-center my-5">
         <h1 class="text-white">La mia prima app con Vite!</h1>
     </div>
 </template>
